a641c9c2bd9eb618ac7e8c5cdbf9e5ab607ac102
laoye <cf850118@163.com>
2016-09-22 17:13:50 +0800

    Added more pointer types & primitive types for decoder.

--- src/io/decoder.rs
+++ src/io/decoder.rs
@@ -12,11 +12,16 @@
  *                                                        *
  * hprose decoder for Rust.                               *
  *                                                        *
- * LastModified: Sep 21, 2016                             *
+ * LastModified: Sep 22, 2016                             *
  * Author: Chen Fei <cf@hprose.com>                       *
  *                                                        *
 \**********************************************************/
 
+use std::rc::Rc;
+use std::sync::Arc;
+use std::borrow::Cow;
+use std::marker::PhantomData;
+use std::cell::{Cell, RefCell};
 use std::hash::{Hash, BuildHasher};
 use std::collections::HashMap;
 
@@ -55,9 +60,33 @@ pub trait Decodable: Sized {
     fn decode<D: Decoder>(d: &mut D) -> Result<Self, D::Error>;
 }
 
+impl Decodable for () {
+    fn decode<D: Decoder>(d: &mut D) -> Result<(), D::Error> {
+        d.read_nil()
+    }
+}
+
 impl Decodable for bool {
     fn decode<D: Decoder>(d: &mut D) -> Result<bool, D::Error> {
         d.read_bool()
+    }
+}
+
+impl Decodable for i8 {
+    fn decode<D: Decoder>(d: &mut D) -> Result<i8, D::Error> {
+        d.read_i64().map(|i| i as i8)
+    }
+}
+
+impl Decodable for i16 {
+    fn decode<D: Decoder>(d: &mut D) -> Result<i16, D::Error> {
+        d.read_i64().map(|i| i as i16)
+    }
+}
+
+impl Decodable for i32 {
+    fn decode<D: Decoder>(d: &mut D) -> Result<i32, D::Error> {
+        d.read_i64().map(|i| i as i32)
     }
 }
 
@@ -67,9 +96,39 @@ impl Decodable for i64 {
     }
 }
 
+impl Decodable for isize {
+    fn decode<D: Decoder>(d: &mut D) -> Result<isize, D::Error> {
+        d.read_i64().map(|i| i as isize)
+    }
+}
+
+impl Decodable for u8 {
+    fn decode<D: Decoder>(d: &mut D) -> Result<u8, D::Error> {
+        d.read_u64().map(|u| u as u8)
+    }
+}
+
+impl Decodable for u16 {
+    fn decode<D: Decoder>(d: &mut D) -> Result<u16, D::Error> {
+        d.read_u64().map(|u| u as u16)
+    }
+}
+
+impl Decodable for u32 {
+    fn decode<D: Decoder>(d: &mut D) -> Result<u32, D::Error> {
+        d.read_u64().map(|u| u as u32)
+    }
+}
+
 impl Decodable for u64 {
     fn decode<D: Decoder>(d: &mut D) -> Result<u64, D::Error> {
         d.read_u64()
+    }
+}
+
+impl Decodable for usize {
+    fn decode<D: Decoder>(d: &mut D) -> Result<usize, D::Error> {
+        d.read_u64().map(|u| u as usize)
     }
 }
 
@@ -97,15 +156,43 @@ impl Decodable for String {
     }
 }
 
-impl<T:Decodable> Decodable for Option<T> {
-    fn decode<D: Decoder>(d: &mut D) -> Result<Option<T>, D::Error> {
-        d.read_option(|d, b| {
-            if b {
-                Ok(Some(Decodable::decode(d)?))
-            } else {
-                Ok(None)
-            }
-        })
+impl<T: Decodable> Decodable for Box<T> {
+    fn decode<D: Decoder>(d: &mut D) -> Result<Box<T>, D::Error> {
+        Ok(Box::new(try!(Decodable::decode(d))))
+    }
+}
+
+impl<T: Decodable> Decodable for Rc<T> {
+    #[inline]
+    fn decode<D: Decoder>(d: &mut D) -> Result<Rc<T>, D::Error> {
+        Ok(Rc::new(try!(Decodable::decode(d))))
+    }
+}
+
+impl<T: Decodable + Send + Sync> Decodable for Arc<T> {
+    fn decode<D: Decoder>(d: &mut D) -> Result<Arc<T>, D::Error> {
+        Ok(Arc::new(try!(Decodable::decode(d))))
+    }
+}
+
+impl<'a, T: ?Sized> Decodable for Cow<'a, T>
+where T: ToOwned, T::Owned: Decodable
+{
+    #[inline]
+    fn decode<D: Decoder>(d: &mut D) -> Result<Cow<'static, T>, D::Error> {
+        Ok(Cow::Owned(try!(Decodable::decode(d))))
+    }
+}
+
+impl<T: Decodable + Copy> Decodable for Cell<T> {
+    fn decode<D: Decoder>(d: &mut D) -> Result<Cell<T>, D::Error> {
+        Ok(Cell::new(try!(Decodable::decode(d))))
+    }
+}
+
+impl<T: Decodable> Decodable for RefCell<T> {
+    fn decode<D: Decoder>(d: &mut D) -> Result<RefCell<T>, D::Error> {
+        Ok(RefCell::new(try!(Decodable::decode(d))))
     }
 }
 
@@ -125,5 +212,23 @@ where K: Decodable + Hash + Eq,
             }
             Ok(map)
         })
+    }
+}
+
+impl<T: Decodable> Decodable for Option<T> {
+    fn decode<D: Decoder>(d: &mut D) -> Result<Option<T>, D::Error> {
+        d.read_option(|d, b| {
+            if b {
+                Ok(Some(Decodable::decode(d)?))
+            } else {
+                Ok(None)
+            }
+        })
+    }
+}
+
+impl<T> Decodable for PhantomData<T> {
+    fn decode<D: Decoder>(d: &mut D) -> Result<PhantomData<T>, D::Error> {
+        d.read_nil().and(Ok(PhantomData))
     }
 }
